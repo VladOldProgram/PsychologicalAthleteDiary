7983ac9aa130ff3670ee3695d50b8db2a646b0bc
Vladislav <86352975+VladOldProgram@users.noreply.github.com>
2023-04-28 22:49:54 +0300

    Add files via upload

---
+++ app/src/main/java/ru/pad/RequestsActivity.java
@@ -0,0 +1,82 @@
+package ru.pad;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ru.pad.objects.Psychologist;
+import ru.pad.objects.Sportsman;
+
+public class RequestsActivity extends AppCompatActivity {
+    private String psychologistUid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requests);
+        getUserData();
+    }
+
+    private void getUserData() {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            psychologistUid = extras.getString("psychologistUid");
+        }
+
+        RecyclerView recyclerViewRequests = findViewById(R.id.recyclerViewRequests);
+        recyclerViewRequests.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewRequests.setHasFixedSize(true);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbUsers = database.getReference("Users");
+
+        DatabaseReference dbUser = dbUsers.child(psychologistUid);
+        dbUser.addValueEventListener(new ValueEventListener() {
+            @Override public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Psychologist psychologist = dataSnapshot.getValue(Psychologist.class);
+                if (psychologist != null && psychologist.getRequests() != null) {
+                    Map<String, String> requests = new HashMap<>(psychologist.getRequests());
+                    List<String> items = new ArrayList<>();
+                    requests.forEach((key, value) -> {
+                        dbUsers.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @SuppressLint("SetTextI18n")
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                Sportsman sportsman = snapshot.getValue(Sportsman.class);
+                                if (sportsman != null
+                                        && sportsman.getName() != null
+                                        && sportsman.getSurname() != null
+                                ) {
+                                    items.add(sportsman.getName() + " " + sportsman.getSurname() + " (" + key.replace("•", ".") + ")");
+                                }
+                                RecyclerViewRequestsAdapter recyclerViewRequestsAdapter = new RecyclerViewRequestsAdapter(items, psychologistUid);
+                                recyclerViewRequests.setAdapter(recyclerViewRequestsAdapter);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) { }
+                        });
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
+    }
+}
